bf70fb197a17bee691181c2edf690460bc0befd6
l33Hart <46654558+l33Hart@users.noreply.github.com>
2019-01-14 00:14:26 -0500

    dev files
    added

---
+++ DVP1.CE1_HartLee/DVP1.CE1_HartLee/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVP1.CE1_HartLee
+{
+    class Program
+    {// Name:Lee Hart
+     // Date:2019 Jan 
+     // Course: Project & Portfolio 1  
+     // Synopsis: this is a project that will have several classes to run one program
+        static void Main(string[] args)
+        {
+        }
+    }
+}
